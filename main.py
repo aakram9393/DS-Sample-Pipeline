d02a1eea6d39ff4faabb1ac71845470f5e819af5
Ahmed Akram <a.akram@zenitharabia.ai>
2024-02-06 07:24:14 +0200

    save model and push it

--- main.py
+++ main.py
@@ -4,11 +4,23 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import joblib
+import os
+
+# Assuming `best_model` is your trained model object
+model_filename = "best_model.joblib"
+
+
+
 
 # Parsing alpha value if passed
 parser = argparse.ArgumentParser()
 parser.add_argument("--alpha", type=float, default=0.5, help="Regularization strength")
 args = parser.parse_args()
+
+model = RandomForestClassifier(max_depth=5)
+# Save the model as 'best_model.joblib' in the current working directory
+joblib.dump(model, os.path.join(os.getcwd(), model_filename)) 
 
 # MLflow tracking URI
 mlflow.set_tracking_uri("http://mlflow:5000")
@@ -33,7 +45,9 @@ with mlflow.start_run():
     
     # Log the sklearn model and register as a version in MLflow model registry
     mlflow.sklearn.log_model(model, "model", registered_model_name="IrisClassifier")
-    
-mlflow.log_artifact("requirements.txt")    
+
+    mlflow.log_artifact(model_filename)
+
+   
  
 print("Model training and logging completed.")
